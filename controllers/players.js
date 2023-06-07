ccdf506815742acaad5af49a9c89724edbce6715
Joe Caruso <carusoj7@gmail.com>
2023-06-07 11:33:41 -0500

    fix backend model issues

--- controllers/players.js
+++ controllers/players.js
@@ -51,20 +51,20 @@ async function show(req, res) {
 async function update(req, res) {
   try {
     const playerId = req.params.playerId;
-    const profile = await Profile.findOne({ where: { userId: req.user.id } });
-    const player = await Player.findOne({ where: { id: playerId, profileId: profile.id } });
+    const profile = await Profile.findOne({ where: { userId: req.user.id } })
+    const player = await Player.findOne({ where: { id: playerId, profileId: profile.id } })
 
     if (!player) {
-      return res.status(404).json({ error: 'Player not found' });
+      return res.status(404).json({ error: 'Player not found' })
     }
     if (player.profileId === profile.id) {
-      await player.update(req.body);
-      res.status(200).json(player);
+      await player.update(req.body)
+      res.status(200).json(player)
     } else {
-      res.status(403).json({ error: 'Unauthorized' });
+      res.status(403).json({ error: 'Unauthorized' })
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
@@ -84,70 +84,66 @@ async function deletePlayer(req, res) {
   }
 }
 
-async function fetchVotes(playerId) {
+async function fetchVotes(req, res) {
   try {
+    const playerId = parseInt(req.params.playerId)
     const player = await Player.findOne({
-      where: { id: playerId },
       attributes: ['upvotes', 'downvotes'],
-    });
+    })
 
     if (!player) {
-      return { upvotes: 0, downvotes: 0 };
+      return { upvotes: 0, downvotes: 0 }
     }
-
+console.log(playerId, player, "back end");
     return {
       upvotes: player.upvotes || 0,
       downvotes: player.downvotes || 0,
-    };
+    }
   } catch (error) {
-    console.log(error);
-    return { upvotes: 0, downvotes: 0 };
+    console.log(error)
   }
 }
 
-async function upvote(playerId, profileId) {
+async function upvote(req, res) {
+  console.log(req.params.playerId);
+  console.log(req.profile);
+  const playerId = parseInt(req.params.playerId)
+  console.log(playerId);
   try {
-    const player = await Player.findOne({ where: { id: playerId } });
-
+    const player = await Player.findOne({ where: { id: playerId } })
     if (!player) {
-      return { upvotes: 0, downvotes: 0 };
+      return res.status(404).json({ message: 'Player not found' })
     }
 
-    if (player.profileId === profileId) {
-      await player.increment('upvotes');
-      return { upvotes: player.upvotes + 1, downvotes: player.downvotes };
-    }
+    player.upvotes++
+    await player.save()
 
-    return { upvotes: player.upvotes, downvotes: player.downvotes };
+    const votes = await fetchVotes(playerId)
+    res.json(votes)
   } catch (error) {
-    console.log(error);
-    return { upvotes: 0, downvotes: 0 };
+    console.error('Error:', error)
+    res.status(500).json({ message: 'Internal server error' })
   }
 }
 
-async function downvote(playerId, profileId) {
+async function downvote(req, res) {
+  const playerId = parseInt(req.params.playerId)
   try {
-    const player = await Player.findOne({ where: { id: playerId } });
-
+    const player = await Player.findOne({ where: { id: playerId } })
     if (!player) {
-      return { upvotes: 0, downvotes: 0 };
+      return res.status(404).json({ message: 'Player not found' })
     }
 
-    if (player.profileId === profileId) {
-      await player.increment('downvotes');
-      return { upvotes: player.upvotes, downvotes: player.downvotes + 1 };
-    }
+    player.downvotes++
+    await player.save()
 
-    return { upvotes: player.upvotes, downvotes: player.downvotes };
+    const votes = await fetchVotes(playerId)
+    res.json(votes)
   } catch (error) {
-    console.log(error);
-    return { upvotes: 0, downvotes: 0 };
+    console.error('Error:', error)
+    res.status(500).json({ message: 'Internal server error' })
   }
 }
-
-
-
-
 module.exports = {
   createPlayer,
   index,
