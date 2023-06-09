56954544417a37efa50fd0dedc610bbc13b87266
Joe Caruso <carusoj7@gmail.com>
2023-06-08 19:09:38 -0500

    complete backend vote functionality

--- controllers/votes.js
+++ controllers/votes.js
@@ -2,21 +2,27 @@ const { Player, Profile, Vote } = require('../models');
 
 async function fetchVotes(req, res) {
   try {
+    const profileId = req.user.profile.id;
+    const playerId = req.params.playerId;
     const {count, rows} = await Vote.findAndCountAll({
-      where: { id: req.params.playerId, vote: 1 },
+      where: { playerId, vote: 1 },
       attributes: ['profileId','vote'],
       
     })  
-    const {downCount, downRows} = await Vote.findAndCountAll({
-      where: { id: req.params.playerId, vote: 0 },
+    const downvotes = await Vote.findAndCountAll({
+      where: { playerId, vote: -1 },
       attributes: ['profileId','vote'],
       
     }) 
+    const existingVote = await Vote.findOne({
+      where: { profileId, playerId }
+    });
+console.log(count, 'upvote');
 console.log('1111111111111111  ');
     res.status(200).json({
-      upvotes: count || 0,
-      downvotes: downCount || 0,
-      playerId: req.params.playerId
+      upvotes: count,
+      downvotes: downvotes.count,
+      existingVote
     })
     return 
   } catch (error) {
@@ -42,16 +48,15 @@ async function upvote(req, res) {
     if (existingVote) {
       existingVote.vote = 1;
       await existingVote.save();
+      res.json(existingVote)
     } else {
-      await Vote.create({
+      const vote = await Vote.create({
         profileId,
         playerId,
         vote: 1
       });
+      res.json(vote)
     }
-
-    const votes = await fetchVotes(req, res);
-    res.json(votes);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ message: 'Internal server error' });
@@ -69,23 +74,22 @@ async function downvote(req, res) {
     });
     console.log(existingVote);
 
-    if (existingVote && existingVote.vote === 0) {
+    if (existingVote && existingVote.vote === -1) {
       return res.status(200).json();
     }
 
     if (existingVote) {
-      existingVote.vote = 0;
+      existingVote.vote = -1;
       await existingVote.save();
+      res.json(existingVote)
     } else {
-      await Vote.create({
+      const vote = await Vote.create({
         profileId,
         playerId,
-        vote: 0
+        vote: -1
       });
+      res.json(vote)
     }
-
-    const votes = await fetchVotes(req, res);
-    res.json(votes);
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ message: 'Internal server error' });
